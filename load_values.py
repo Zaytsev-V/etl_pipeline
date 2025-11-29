1e660476cf28fb64ed3043167bf44fe84e0b224d
unknown <yplessons@gmail.com>
2025-11-29 12:08:04 +0300

    add some columns

--- load_values.py
+++ load_values.py
@@ -164,23 +164,29 @@ def main():
 
     # создаю новый индикатор, расчитываю его значения на душу населения, добавляю строки в датафрейм
     # ищу значения популяции для каждого года и страны
-    pop_data = df[df['indicator'] == 'Population, total'][['country', 'year', 'value']]
+    pop_data = df[df['indicator'] == 'Population, total'][['country_id', 'country', 'year', 'value']]
     pop_data = pop_data.rename(columns={'value': 'population'})
 
     # ищу значения научных статей для каждого года и страны
-    scy_art_data = df[df['indicator'] == 'Scientific and technical journal articles'][['country', 'year', 'value']]
+    scy_art_data = df[df['indicator'] == 'Scientific and technical journal articles'][['country_id', 'country',
+                                                                                       'year', 'value']]
     scy_art_data = scy_art_data.rename(columns={'value': 'publication'})
 
     # объединяю данные
-    merged_data = scy_art_data.merge(pop_data, on=['country', 'year'], how='inner')
+    merged_data = scy_art_data.merge(pop_data, on=['country_id', 'country', 'year'], how='inner')
 
     # вычисляю кол-во публикаций на душу населения
     merged_data['publication_per_capita'] = merged_data['publication'] / merged_data['population']
 
     # создаю новые строки для добавления в основной датафрейм
-    new_rows = merged_data[['country', 'year']].copy()
+    new_rows = merged_data[['country_id', 'country', 'year']].copy()
+    new_rows['indicator_id'] = 'custom_indicator'  # новый ID для нового индикатора
     new_rows['indicator'] = 'Sci. and tech. journal articles per capita'  # название нового индикатора
     new_rows['value'] = merged_data['publication_per_capita']
+
+    # делаю порядок столбцов как в основным DataFrame
+    correct_order = ['country_id', 'country', 'indicator_id', 'indicator', 'year', 'value']
+    new_rows = new_rows[correct_order]
 
     # добавляю новые строки к датафрейму
     df = pd.concat([df, new_rows], ignore_index=True)
